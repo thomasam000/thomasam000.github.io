8fe22f2e72e2687dd644ca89432ac962f0f040d8
thomasam000 <thomasam000@gmail.com>
2022-07-27 11:09:24 -0400

    new portfolio page from scratch

--- portfolio-2.0/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '@/router'
-
-const my_app = createApp(App)
-my_app.use(router)
-my_app.mount('#app')
---
+++ static/vue/pages/home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        Home
+        <input type="number" v-model="number">    
+    </div>
+    
+</template>
+
+<script>
+module.exports = {
+    components: {
+
+    },
+    data: function() {
+        return { 
+            number:0
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    async mounted() {
+
+    },
+    beforeDestroy() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
